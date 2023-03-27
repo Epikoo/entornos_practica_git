b120b90d95dfd305d08e7b2ea09a25b8f0d2a072
unknown <ignacioandresdiazromero@gmail.com>
2023-03-27 10:45:47 -0700

    Realizacion del metodo de factorizacion

--- com.nachoDiaz.algoritmo/src/com/nachoDiaz/algoritmo/Algoritmo.java
+++ com.nachoDiaz.algoritmo/src/com/nachoDiaz/algoritmo/Algoritmo.java
@@ -1,7 +1,9 @@
 package com.nachoDiaz.algoritmo;
+
 /**
  * 
  * Es una clase para realizar distintos calculos matematicos
+ * 
  * @author Nacho
  * @version 1.0
  *
@@ -10,26 +12,42 @@ public abstract class Algoritmo {
 	/**
 	 * 
 	 * Calcula el valor de un numero dentro de la serie de fibonacci
+	 * 
 	 * @param numero
 	 * @return valor del numero en la serie de fibonacci
 	 */
-public static int fibonacci(int numero) {
+	public static int fibonacci(int numero) {
 
-    if (numero == 0)
-        return 0;
-    if (numero == 1 || numero == 2)
-        return 1;
+		if (numero == 0)
+			return 0;
+		if (numero == 1 || numero == 2)
+			return 1;
 
-    int anterior = -1;
-    int res = 1;
+		int anterior = -1;
+		int res = 1;
 
-    for (int i = 0; i <= numero; i++) {
+		for (int i = 0; i <= numero; i++) {
 
-        int sum = res + anterior;
-        anterior = res;
-        res = sum;
-    }
+			int sum = res + anterior;
+			anterior = res;
+			res = sum;
+		}
 
-    return res;
-    }
+		return res;
+	}
+	/**
+	 * Calcula el factorial de un numero
+	 * @param numero
+	 * @return factorial del numero
+	 */
+
+	public static int factorial(int numero) {
+		int res = 1;
+
+		for (int i = 2; i <= numero; i++) {
+			res *= i;
+		}
+
+		return res;
+	}
 }
