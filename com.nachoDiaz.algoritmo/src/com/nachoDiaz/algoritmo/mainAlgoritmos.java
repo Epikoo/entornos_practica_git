29579b796c9320cad0348ee40a86c24eb8fb1964
unknown <ignacioandresdiazromero@gmail.com>
2023-03-27 12:17:16 -0700

    Main de pruebas creado

---
+++ com.nachoDiaz.algoritmo/src/com/nachoDiaz/algoritmo/mainAlgoritmos.java
@@ -0,0 +1,18 @@
+package com.nachoDiaz.algoritmo;
+
+public class mainAlgoritmos {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		int numeros[] = new int[5];
+		long resultadoFibonacci[] = new long[numeros.length];
+		long resultadoFactorial[] = new long[numeros.length];
+		boolean resultadoPrimos[] = new boolean[numeros.length];
+		for (int i = 0; i < numeros.length; i++) {
+			numeros[i] = (int) (Math.random() * 20 + 1);
+			resultadoFibonacci[i] = Algoritmo.fibonacci(numeros[i]);
+			resultadoFactorial[i] = Algoritmo.factorial(numeros[i]);
+			resultadoPrimos[i] = Algoritmo.primo(numeros[i]);
+		}
+	}
+}
